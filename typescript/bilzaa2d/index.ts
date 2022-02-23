518f412787fa76aeefc55dee3366df30a54672d5
skilzaa <skillzaa.com@gmail.com>
2022-02-23 14:06:15 +0500

    improved a bit

--- typescript/bilzaa2d/index.ts
+++ typescript/bilzaa2d/index.ts
@@ -3,7 +3,6 @@ import Style from "./pack/style.js";
 import Bilzaa2d from "./../bilzaa2d/bilzaa2d.js";
 import Component from "./component.js";
 import Position from "./design/Position.js";
-import Transition from "../transition/transition.js";
 import {DrawLayer} from "./design/drawLayer.js";
 import {XAlignment,YAlignment} from "./design/alignment.js";
 import IDrawable from "./design/IDrawable.js";
@@ -14,7 +13,6 @@ Pack,
 DrawLayer,
 Style,
 Component,
-Transition,
 XAlignment,
 YAlignment,
 Position,
