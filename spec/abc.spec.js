0e885a5d7ba5c4eb2898058f0c0b3d2b5462304c
skillzaa <skillzaa.com@gmail.com>
2022-03-21 20:30:30 +0500

    uploading as bilza rather than bilzaa2d

--- spec/abc.spec.js
+++
@@ -1,7 +0,0 @@
-// import Bilzaa2d from "../build/bundle.js";
-let bilzaa = require("../build/bundle_cjs");
-describe("my first suite", function() {
-    it("First suite", function() {
-      expect(true).toBe(true);
-    });
-});
