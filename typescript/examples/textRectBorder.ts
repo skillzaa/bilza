83a5efaacbf365caa184bef442cd7c12727f0e07
skillzaa <skillzaa.com@gmail.com>
2022-03-26 02:09:10 +0500

    moving

--- typescript/examples/textRectBorder.ts
+++ typescript/examples/textRectBorder.ts
@@ -4,6 +4,7 @@ let bil = new Bilza("bilza",600, 1200, 1000);
 // bil.add.grid(0,15000);
 // bil.add.counter(0,0,15000);
 bil.add.text("Bilza Text Templates", "black",40, 0, bil.getTimeEnd(), 50, 20);
-
 bil.add.textTempl.shadowRect("Bilza.js",0,20000,"red","red",80);
+
+
 bil.draw();
