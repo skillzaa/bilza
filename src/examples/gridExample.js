686e81868ef671e7c00562a3e63a69cf5b90f18c
skillzaa <skillzaa.com@gmail.com>
2022-03-26 18:41:17 +0500

    moving on

--- src/examples/gridExample.js
+++ src/examples/gridExample.js
@@ -1,4 +1,4 @@
 import Bilza from "../index.js";
 let bil = new Bilza("bilza", 800, 300, 1000);
-bil.add.gridTempl.dashedGrid();
+bil.add.grid();
 bil.draw();
--- typescript/examples/gridExample.ts
+++ typescript/examples/gridExample.ts
@@ -1,7 +1,7 @@
 import Bilza from "../index.js";
 
 let bil = new Bilza("bilza",800, 300, 1000);
-bil.add.gridTempl.dashedGrid();
+bil.add.grid();
 // bil.add.counter(0,0,15000);
 
 
