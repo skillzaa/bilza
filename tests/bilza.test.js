365e596192ae82861425b9bdce000c74f2285c70
skillzaa <skillzaa.com@gmail.com>
2022-03-27 19:06:47 +0500

    now jest can use es6 modules thus i do not need the cjs build

--- tests/bilza.test.js
+++ tests/bilza.test.js
@@ -1,7 +1,11 @@
 "use strict";
-const bilza = require('../build/bilza_cjs');
-const style = new bilza.Style(0);
-style.
-    test('Adds 1 + 2 to equal 3', () => {
-    expect(style.fontSize).toEqual(25);
+// const bilza = require('../build/bilza_cjs');
+
+import Bilza from "../src/index.js";
+const bilza = new Bilza();
+// style.
+console.log(bilza);
+
+test('Adds 1 + 2 to equal 3', () => {
+    expect(bilza.background.color).toBe("#efeee3");
 });
