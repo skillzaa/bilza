721e280547bf691ef2dbc6a6102411982ec1a569
skillzaa <skillzaa.com@gmail.com>
2022-03-29 11:13:12 +0500

    version0.0.11

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import { terser } from 'rollup-plugin-terser';
+
+const devMode = (process.env.NODE_ENV === 'development');
+// console.log(`${ devMode ? 'development' : 'production' } mode bundle`);
+
+export default [
+  {
+
+    input: './src/Bilza.js',
+
+    output: {
+      file: './build/Bilza_0_0_11.js',
+      format: 'es',
+      sourcemap: devMode ? 'inline' : false,
+      plugins: [
+        terser({
+          ecma: 2020,
+          mangle: { toplevel: true },
+          compress: {
+            module: true,
+            toplevel: true,
+            unsafe_arrows: true,
+            drop_console: !devMode,
+            drop_debugger: !devMode
+          },
+          output: { quote_style: 1 }
+        })
+      ]
+    }
+
+  }
+];
--- src/examples/gridExamples/gridExample.js
+++ src/examples/gridExamples/gridExample.js
@@ -1,4 +1,4 @@
-import Bilza from "../../Bilza.js";
+import Bilza from "./Bilza_0_0_11.js";
 let bil = new Bilza("bilza", 800, 300, 1000);
 let g = bil.add.gridTempl.simple("blue");
 bil.draw();
