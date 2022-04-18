f30456f749dd3d4616368848b0d4b0fbfbbb7e69
skillzaa <skillzaa.com@gmail.com>
2022-04-19 02:03:07 +0500

    slide made dont expect it to be perfect right now

--- examples/text/slide.js
+++ examples/text/slide.js
@@ -1,14 +1,15 @@
 import Bilza from "../../src/Bilza.js";
 
-let bil = new Bilza("bilza",500,300);
-// bil.dynamicCanvas(90,90);
+let bil = new Bilza("bilza");
+bil.dynamicCanvas(90,90);
 
-let g = bil.add.rGrid();
-g.d.flagDrawNumbers = true;
-g.d.fontSize = 14;
+// let g = bil.add.rGrid();
+let g = bil.add.gridTempl.dashed();
+// g.d.flagDrawNumbers = true;
+// g.d.fontSize = 14;
 
-let slide = bil.add.slideHnL("The is the Title should heat happened to keeping it with in the limit of"); 
-slide.addItem("this is fist item which i am talking about in the name of");
-slide.addItem("this is second item asdf vfnbg fjkdsd rgl;ks lskf;ls lskflks slfksldkf xc,mcvslkdfvsl");
+let slide = bil.add.slideHnL("The is the Title should heat"); 
+slide.addItem("this is fist item which i am talking ");
+slide.addItem("this is second item ");
 slide.addItem("this is third item");
 bil.start();
