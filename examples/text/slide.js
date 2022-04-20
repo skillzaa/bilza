37594961ffdfbe951a7824d0ef3c6afcaf1948be
skillzaa <skillzaa.com@gmail.com>
2022-04-20 14:17:10 +0500

    just make it

--- examples/text/slide.js
+++ examples/text/slide.js
@@ -18,6 +18,7 @@ slide02.addItem("Items are random and has no sequence",true,5);
 slide02.addItem("The store is locked but not secured",true,10);
 slide02.addItem("All the items are for sale",true,15);
 ///////////////////////////////////////////////////////////
+
 let rng = document.getElementById("range");
 rng.style.width = window.innerWidth;  
 rng.addEventListener("change",function(e){
