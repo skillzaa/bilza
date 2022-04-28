1ab8dd585d2852d0de93c4f4fb4037fd0fb83d84
skillzaa <skillzaa.com@gmail.com>
2022-04-28 15:46:40 +0500

    next dynamicfontsize 02 and then para / lines component

--- docs/text/dynamicFontSize.js
+++ docs/text/dynamicFontSize.js
@@ -11,7 +11,10 @@ bil.draw();
 let dims = 0;
 let inter = setInterval(function(){
     dims +=10;
-    if (dims >= 100) { clearInterval(inter);}
+    if (dims >= 100) { 
+        bil.setCanvas(400,150);
+        clearInterval(inter);
+    }
 bil.dynamicFontSize(txt01,dims,dims);
 bil.draw();
 },1000);
