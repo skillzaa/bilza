bf2c31057504eebf90603dddf01198eeeaee1a18
skillzaa <skillzaa.com@gmail.com>
2022-05-10 18:48:04 +0500

    version 0.0.15

--- samples/circle/index.js
+++ samples/circle/index.js
@@ -1,13 +1,18 @@
-// import Bilza from "../00lib/Bilza_0_0_14.js";
-import Bilza from "../../src/Bilza.js";
+import Bilza from "../00lib/Bilza_0_0_15.js";
+// import Bilza from "../../src/Bilza.js";
 
 let bil = new Bilza("bilza",60,1000,500);
-bil.gridTempl.simple("grey");
-
+bil.background.color = "#ffff00";
 bil.add.circle(0,300,15,10,10,"#008000",true);
 //////////////////////////////////
 bil.draw();
 ///////////////////////////////
-// let inter = setInterval(function(){    
-
-// },50);
+let inter = setInterval(function(){    
+bil.add.circle(0,300,
+    Math.floor(Math.random() * 100),
+    Math.floor(Math.random() * 100),
+    10,
+    "#008000",
+    true);
+bil.draw();
+},500);
