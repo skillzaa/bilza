2fec7559a11b85206717b3d1803fe7ecb57dd24c
skillzaa <skillzaa.com@gmail.com>
2022-05-16 23:50:05 +0500

    insert.append tests made

--- tests/bilzaDuration.test.js
+++ tests/bilzaDuration.test.js
@@ -5,22 +5,17 @@
 // import Text from "../src/compsMod/components/text/text.js";
 // import Grid from "../src/compsMod/components/grid/grid.js";
 import Duration from "../src/engine/duration.js"
-/**
- * Test the basics all important pieces should be there
- */
+
 let dur = new Duration();
-
-// let tst = new TestComp(12);
-// let g = new Grid("#0000ff");
-// let t = new Text(20,"Seperate engine");
-
-// bil.insert(tst);
-// bil.insert(g);
-// bil.insert(t);
 
 test('bil.duration.len() = 0', () => {
 // console.log("_pvt_duration_val", dur._pvt_duration_val);
 expect(dur.len()).toBe(0);
+});
+
+test('duration.entend', () => {
+// console.log("_pvt_duration_val", dur._pvt_duration_val);
+expect(dur.extend(10)).toBe(10);
 });
 
 
---
+++ tests/insert_append.test.js
@@ -0,0 +1,45 @@
+import Bilza from "../src/Bilza.js";
+import TestComp from "../src/compsMod/components/testComp/testComp.js"
+
+test('Append Basic', () => {
+    let bil  =  new Bilza();
+    let a = new TestComp();
+let b = new TestComp();
+bil.insert.append(a,10);    
+bil.insert.append(b,5);    
+expect(bil.len(false)).toBe(15);
+});
+
+test('Try to append comp with zero duration', () => {
+    let bil  =  new Bilza();    
+let c = new TestComp();
+// c.insertType = c.insertTypeOptions.Append;
+expect(()=>{ bil.insert.append(c,0)}).toThrow("for Insert operation to succeed you need component duration greater than 0");
+});
+
+test('3 X comp-Append duration 30', () => {
+let bil = new Bilza();
+let c = new TestComp();
+let d = new TestComp();
+let e = new TestComp();
+
+bil.insert.append(c,10);    
+bil.insert.append(d,10);    
+bil.insert.append(e,10);    
+// console.log("_pvt_duration_val", dur._pvt_duration_val);
+expect(bil.len(false)).toBe(30);
+});
+
+test('3 X comp-Append duration different tmiings (total time =18)', () => {
+let bil = new Bilza();
+let c = new TestComp();
+let d = new TestComp();
+let e = new TestComp();
+
+bil.insert.append(c,8);    
+bil.insert.append(d,6);    
+bil.insert.append(e,4);    
+expect(bil.len(false)).toBe(18);
+});
+
+
