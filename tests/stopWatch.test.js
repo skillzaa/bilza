6eb507742de008049a1a213e51a602d16c608762
skillzaa <skillzaa.com@gmail.com>
2022-05-17 01:11:45 +0500

    bilza engine testing completed for now - now write basic docs and wind up

---
+++ tests/stopWatch.test.js
@@ -0,0 +1,31 @@
+
+// import Bilza from "../src/Bilza.js"
+import StopWatch from "../src/bilzaMod/stopWatch.js";
+
+
+
+test('check stop watch', () => {
+let s = new StopWatch();
+    expect(s.isAlreadyRunning()).toBeFalsy();
+});
+
+test('getMsDelta', () => {
+let s = new StopWatch();
+let getMsDelta = s.getMsDelta();
+    expect(getMsDelta).toBe(0);
+});
+//--;ater since it needs timer
+// test('start', () => {
+// let s = new StopWatch();
+// s.start();
+//     // console.log("getMsDelta",getMsDelta);
+//     expect(s.getMsDelta()).toBeGreaterThan(0);
+// });
+
+test('start-stop', () => {
+let s = new StopWatch();
+s.start();
+    s.stop();
+    expect(s.getMsDelta()).toBe(0);
+});
+
--- typescript/bilzaMod/stopWatch.ts
+++ typescript/bilzaMod/stopWatch.ts
@@ -1,5 +1,4 @@
 
-
 export default class StopWatch {
 private interval : number | null; 
 //??
