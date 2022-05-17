6076f0215358891900fcb7e1d80d405ed8e3dad0
skillzaa <skillzaa.com@gmail.com>
2022-05-17 17:58:09 +0500

    finally a very simple BaseComponent class - no generics no seperate data required HOWEVER the properties of Basecomp are in this.props

--- typescript/testComp/testComp.ts
+++ typescript/testComp/testComp.ts
@@ -1,6 +1,5 @@
 import {Pack} from "../Bilza.js";
 import Component from "../BaseComponent/Basecomponent.js";
-import DataFn,{ObjectData} from "./DataFn.js";
  
 export default class TestComp extends Component {
     msDelta :number;  
@@ -24,6 +23,5 @@ draw(p:Pack):boolean{
 p.drawText(this.msDelta.toString(),this.p.x,5 ,this.style);
 return true;
 }
-
 
 }
