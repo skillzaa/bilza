95e0471394cccc468c6cce65eb0b4ea5f863aeaf
skillzaa <skillzaa.com@gmail.com>
2022-05-19 17:57:33 +0500

    now introducing the components back one by one

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import Bilza from "../src/Bilza.js";
 // import Bilza from "../../build/bilzaEngine_0_0_15.js";
 import testClip from "./clip.js";
 
-// import TestComp from "./src/testComp/testComp.js";
 let bil = new Bilza("bilza");
 let first = testClip("first",0);
 bil.insert.append(first,first.duration);
 first.moveX(5,10,0,200);
 
-let tst = testClip("tst",50); 
+let tst = testClip("Bilal",50); 
 bil.insert.append(tst,tst.duration);
 tst.moveX(5,10,0,200);
 
-let tst02 = testClip("tst02",100); 
+let tst02 = testClip("Mustafa",100); 
 bil.insert.append(tst02,tst02.duration);
-tst02.moveX(5,10,0,200);
+tst02.moveX(5,10,0,400);
+
+let tst03 = testClip("tst03",150); 
+bil.insert.append(tst03,tst03.duration);
+tst03.moveX(5,10,0,200);
 //--////////////////////////////
 bil.start();
 ///////////////////////////////
