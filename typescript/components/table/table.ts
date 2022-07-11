01c9c453fb567a8cd4ae572ba740d36fc1b94e8b
skillzaa <skillzaa.com@gmail.com>
2022-07-11 17:04:50 +0500

    ok

---
+++ typescript/components/table/table.ts
@@ -0,0 +1,61 @@
+import Pack from "../../pack/pack.js";
+import BaseComponent from "../../BaseComponent/00BaseComponent.js";
+
+export default class Table extends BaseComponent {
+  
+constructor (color :string="#000000"){ 
+    super();
+    this.color.set(color) ; 
+}
+
+init(p: Pack): boolean {
+    super.init(p);
+    return true;
+}
+
+update(msDelta: number, p: Pack): boolean {
+    super.update(msDelta,p);
+    return true;
+}
+widthInPix(): number {
+    if (this.canvasWidth !== null ){
+        return Math.ceil((this.canvasWidth/100) * this.width.value());
+    }else {
+        throw new Error("the component is not initialized yet");        
+    }
+}
+heightInPix(): number {
+    if (this.canvasHeight !== null){
+    return Math.ceil((this.canvasHeight/100)*this.height.value());
+    }else {
+        throw new Error("the component is not initialized yet");        
+    }
+}
+draw(p:Pack):boolean{
+
+this.applyRotation(p);
+//--------------
+this.style.fillStyle = this.color.value();    
+this.style.opacity = this.opacity.value();
+// console.log("this.opacity.value()/100",this.opacity.value()/100);
+// p.applyOpacity(this.opacity.value());
+this.style.strokeStyle = this.color.value(); 
+// console.log("this.xRotateAligned()",this.xRotateAligned());
+// console.log("this.xAligned()",this.xAligned());
+//---------------------------------------
+   p.drawFillRect(
+     this.xAligned(),
+     this.yAligned(),
+
+    this.widthInPix(),
+    this.heightInPix(),
+    this.style
+    );
+//----------------------------
+this.removeRotation(p);
+this.style.opacity = 1;
+//----------------------------
+return true;
+}
+
+}
