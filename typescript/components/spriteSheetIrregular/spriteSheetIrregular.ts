47c37bafeb4f26e8f76ce3239027fe688d1fde91
skillzaa <skillzaa.com@gmail.com>
2022-07-28 00:45:53 +0500

    spritesheet irregular

--- typescript/components/spriteSheetIrregular/spriteSheetIrregular.ts
+++ typescript/components/spriteSheetIrregular/spriteSheetIrregular.ts
@@ -23,21 +23,18 @@ this.imagesList.push(new ImageData("angel",263,20,135,116));
 this.imagesList.push(new ImageData("redDeamon",396,16,130,123));
 this.imagesList.push(new ImageData("robot",529,15,121,119));
 this.imagesList.push(new ImageData("windows",647,22,121,123));
-
 this.imagesList.push(new ImageData("military",26,140,118,130));
 this.imagesList.push(new ImageData("candy",144,140,109,130));
 this.imagesList.push(new ImageData("chocolate",269,140,134,130));
 this.imagesList.push(new ImageData("cowboy",402,140,126,130));
 this.imagesList.push(new ImageData("bowtie",525,140,128,130));
 this.imagesList.push(new ImageData("wall",653,140,123,130));
-
 this.imagesList.push(new ImageData("cat",27,272,121,130));
 this.imagesList.push(new ImageData("chinese",146,260,118,130));
 this.imagesList.push(new ImageData("japnese",257,279,139,121));
 this.imagesList.push(new ImageData("kid",398, 273,121,121));
 this.imagesList.push(new ImageData("ninja",523,269,121,123));
 this.imagesList.push(new ImageData("pirate",640, 281,123,111));
-
 this.imagesList.push(new ImageData("nurse",29,400,114,132));
 this.imagesList.push(new ImageData("cop",140,400,116,128));
 this.imagesList.push(new ImageData("santa",272,400,121,125));
--- typescript/try.ts
+++ typescript/try.ts
@@ -3,35 +3,84 @@ import SpriteSheetIrregular from "./components/spriteSheetIrregular/spriteSheetI
 
 let bil = new Bilza("bilza", 70);
 
-//-----
-
+bil.background.color.set(hsl(240,100,85));
 const fc = cf.frameCounter();
 fc.goto(0,70,0)
 bil.insert.alwaysOn(fc);
 
-//-----
-
-const grid = cf.grid(hsl(240,100,20));
-bil.insert.alwaysOn(grid);
+// const grid = cf.grid(hsl(240,100,20));
+// bil.insert.alwaysOn(grid);
 
 const ss = new SpriteSheetIrregular("../images/ghosts.png");
-bil.insert.add(ss,0,60);
+ss.xAlign = ss.XAlignOpt.Mid;
+ss.yAlign = ss.YAlignOpt.Mid;
 
+ss.x.set(50);
 ss.y.set(50);
-ss.x.animate(2,10,0,70);
 
-ss.gotoImage(3,"nurse");
-ss.gotoImage(5,"cop");
-ss.gotoImage(7,"santa");
-ss.gotoImage(9,"queen");
-ss.gotoImage(11,"punk");
-ss.gotoImage(13,"checker");
-ss.gotoImage(15,"stripes");
-ss.gotoImage(17,"glasses");
+ss.gotoImage(0, "blue");
+ss.gotoImage(2, "greenAlien");
+ss.gotoImage(4, "angel");
+ss.gotoImage(6, "redDeamon");
+ss.gotoImage(8, "robot");
+ss.gotoImage(10,"windows");
+ss.gotoImage(12,"military");
+ss.gotoImage(14,"candy");
+ss.gotoImage(16,"chocolate");
+ss.gotoImage(18,"cowboy");
+ss.gotoImage(20,"bowtie");
+ss.gotoImage(22,"wall");
+ss.gotoImage(24,"cat");
+ss.gotoImage(26,"chinese");
+ss.gotoImage(28,"japnese");
+ss.gotoImage(30,"kid");
+ss.gotoImage(32,"ninja");
+ss.gotoImage(34,"pirate");
+ss.gotoImage(36,"nurse");
+ss.gotoImage(38,"cop");
+ss.gotoImage(40,"santa");
+ss.gotoImage(42,"queen");
+ss.gotoImage(44,"punk");
+ss.gotoImage(46,"checker");
+ss.gotoImage(48,"stripes");
+ss.gotoImage(50,"glasses");
 
-// ss.currentImage.goto(2,1);
-// ss.currentImage.goto(4,2);
-// ss.currentImage.goto(6,3);
+bil.insert.add(ss,0,60);
+//-------------------------------
+const txt = cf.rawText("Sprite sheet", hsl(0));
+txt.xAlign = txt.XAlignOpt.Mid;
+txt.x.set(50);
+txt.y.set(80);
+txt.width.set(50);
+bil.insert.add(txt,0,60);
+//-------------------------------
+txt.content.goto(0,"blue");
+txt.content.goto(2,"greenAlien");
+txt.content.goto(4,"angel");
+txt.content.goto(6,"redDeamon");
+txt.content.goto(8,"robot");
+txt.content.goto(10,"windows");
+txt.content.goto(12,"military");
+txt.content.goto(14,"candy");
+txt.content.goto(16,"chocolate");
+txt.content.goto(18,"cowboy");
+txt.content.goto(20,"bowtie");
+txt.content.goto(22,"wall");
+txt.content.goto(24,"cat");
+txt.content.goto(26,"chinese");
+txt.content.goto(28,"japnese");
+txt.content.goto(30,"kid");
+txt.content.goto(32,"ninja");
+txt.content.goto(34,"pirate");
+txt.content.goto(36,"nurse");
+txt.content.goto(38,"cop");
+txt.content.goto(40,"santa");
+txt.content.goto(42,"queen");
+txt.content.goto(44,"punk");
+txt.content.goto(46,"checker");
+txt.content.goto(48,"stripes");
+txt.content.goto(50,"glasses");
+//-------------------------------
 const ui = new Ui(bil);
 bil.draw(); //-------------------------------------//
 /////////////////////////////////////////////////////
