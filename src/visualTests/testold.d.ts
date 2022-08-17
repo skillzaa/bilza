082134c022ad25f8807004119dff46e2a9118c45
skillzaa <skillzaa.com@gmail.com>
2022-08-17 10:48:52 +0500

    preared the project for maintainance mode --visual tests folder created and first 2 visual tests added

--- src/visualTests/testold.d.ts
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=testold.d.ts.map
--- typescript/try.ts
+++ typescript/try.ts
@@ -1,23 +1,28 @@
-import Bilza, { Ui, CompFactory as cf, hsl, k } from "./bilza.js";
+import Bilza, { Ui, CompFactory as cf, hsl} from "./bilza.js";
+
 let bil = new Bilza("bilza", 70);
 const grid = cf.grid("grey");
 
 bil.insert.alwaysOn(grid);
-// bil.insert.alwaysOn(cf.frameCounter(hsl(0)));
-const txt = cf.fillRect( hsl(240));
 
-txt.width.set(30);
-txt.height.set(30);
-
-txt.x.set(50);
-txt.y.set(50);
-//---first time use
-txt.align(1,1);
-txt.alignRotate(0,2);
-// txt.x.animate(1,5,0,90);
-txt.rotation.animate(1,5,0,500);
-bil.insert.add(txt, 0, 15);
+const txt = cf.fillRect(hsl(240));
+txt.x.set(40);
+txt.y.set(20);
+//--if we start animation from frame 0 it will overwrite x.set and y.set
+txt.x.animate(2, 6, 80, 0);
+txt.x.goto(10, 10);
+txt.x.goto(15, 20);
+txt.x.goto(20, 30);
+txt.x.goto(25, 40);
+txt.x.goto(30, 50);
+txt.x.goto(35, 60);
+txt.y.goto(40, 20);
+txt.y.goto(45, 30);
+txt.y.goto(50, 40);
+txt.y.goto(55, 50);
+txt.y.goto(60, 60);
+txt.y.goto(65, 70);
+bil.insert.add(txt, 0, 80);
 const ui = new Ui(bil);
 bil.draw();
-
-console.log("txt",txt);
+// console.log("txt", txt);
