e6286f174dcb3824914dc6d2419b9b27835c278a
skillzaa <skillzaa.com@gmail.com>
2022-08-28 15:44:46 +0500

    next stop testing text comp and creating textTempl

---
+++ src/components/lineR.d.ts
@@ -0,0 +1,17 @@
+import { Pack, BaseComponent } from "../bilza.js";
+import { AniNumber, AniPerc } from "../animationModule/animations.js";
+export default class LineR extends BaseComponent {
+    x2: AniPerc | AniNumber;
+    y2: AniPerc | AniNumber;
+    lineWidth: AniNumber;
+    constructor(x1?: number, y1?: number, x2?: number, y2?: number, color?: string);
+    setRespLoc(tf?: boolean): boolean;
+    init(p: Pack): boolean;
+    update(msDelta: number, p: Pack): boolean;
+    draw(p: Pack): boolean;
+    compWidth(): number;
+    compHeight(): number;
+    align(x?: number | null, y?: number | null): void;
+    alignRotate(x?: number | null, y?: number | null): void;
+}
+//# sourceMappingURL=lineR.d.ts.map
