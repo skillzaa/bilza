8600da7bdfb5e0befb44fdd65e9bd05a273611e9
skillzaa <skillzaa.com@gmail.com>
2022-09-03 17:45:08 +0500

    going to docs

--- typescript/try.ts
+++ typescript/try.ts
@@ -1,19 +1,30 @@
 import Bilza, {Slide,Ui, CompFactory as cf,hsl,getRandom,Scene} from "./bilza.js";
-
 const bil = new Bilza("bilza",70);
-bil.background.color.set("blue");
+bil.background.color.set("darkblue");
+//--Step-2 Creating Elements
 
-bil.insert.add( cf.grid(10,10,hsl(0))  , 0 , 30 );
-bil.insert.add( cf.frameCounter(hsl(60))  , 0 , 30 );
+//-- create a Grid Element
+const grid = cf.grid(10,10, "grey");
+//--insert.add will make the gird show from second 0 to 15.
+bil.insert.add( grid, 0 , 15 );
 
-const txt = cf.text("Demo Text");
-bil.insert.add(txt,2,6);
+//---create a frameCounter component that counts running seconds.
+const frameCounter = cf.frameCounter( "yellow" );
+bil.insert.add( frameCounter  , 0 , 15 );
+//--------------------
+
+//--create Text element
+
+const txt = cf.text("Welcome To Bilza.js");
+
+bil.insert.add(txt,2,8);
+
 
 txt.templ.jumbotron();
-txt.theme.color( 240 );
-txt.entryAni.fadeIn()
+txt.theme.color( 0 );
+txt.entryAni.topIn()
 txt.exitAni.fadeOut();
 
+//--------------------
 const ui = new Ui(bil);
 bil.draw();
-
