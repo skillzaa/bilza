d97419011a7c66d9f4b05eeea3d02d5f740c865e
skillzaa <skillzaa.com@gmail.com>
2022-09-28 21:05:33 +0500

    one up

--- typescript/try.ts
+++ typescript/try.ts
@@ -4,6 +4,13 @@ let bil = new Bilza("bilza", 70);
 
 bil.alwaysOn().grid(10,10,hsl(0,80,20));
 // bil.alwaysOn().frameCounter(0);
+// bil.alwaysOn().frameCounter(0);
+// bil.alwaysOn().frameCounter(0);
+// bil.alwaysOn().frameCounter(0);
+// bil.alwaysOn().frameCounter(0);
+// bil.alwaysOn().frameCounter(0);
+// bil.alwaysOn().frameCounter(0);
+// bil.alwaysOn().frameCounter(0);
 // bil.background.color.set(hsl(240,30,10));
 //=============================================
 // const title = bil.add(0,15).text("Markers",hsl(120));
